d630ee2fe6252b14d840eb6ae0c2a82271c02ed7
praveen.bamane@brose.com <praveen.bamane@brose.com>
2020-08-27 10:57:37 +0530

    1. Solution Created

---
+++ PraveenBamane_Assignment_Maersk/PraveenBamane_Assignment_Maersk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PraveenBamane_Assignment_Maersk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
