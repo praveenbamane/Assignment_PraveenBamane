29a0ed1268c396f94878a3bff4a301ff6f352b98
praveen.bamane@brose.com <praveen.bamane@brose.com>
2020-08-27 11:12:35 +0530

    4. Added class for Rules

---
+++ PraveenBamane_Assignment_Maersk/PraveenBamane_Assignment_Maersk/RuleBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PraveenBamane_Assignment
+{
+   public class RuleBase
+    {
+
+        public PackingSlip Generatepackingslip(string orderdetails)
+        {
+            PackingSlip _objpackingslip = new PackingSlip();
+            _objpackingslip.PackingSlipid = generateMemberID(10001);
+            _objpackingslip.type = Enums.PackingType.Original;
+            _objpackingslip.addedservice = null;
+            return _objpackingslip;
+        }
+
+
+
+    }
+}
